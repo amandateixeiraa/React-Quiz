28a19372416433c1c4a6ac9cb18d3669a45d2ef7
Amanda Teixeira <104781128+amandateixeiraa@users.noreply.github.com>
2025-01-17 11:32:33 -0600

    style update

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import FirstPage from './page/FirstPage/FirstPage';
 import SecondPage from './page/SecondPage/SecondPage';
 import ThirdPage from './page/ThirdPage/ThirdPage';
+import nnnoiseImage from './assets/nnnoise1.svg';
 
 function App(){
   return(
+    <div>
+    <div className = "overlay">
+    <img src={nnnoiseImage} alt="Overlay" className='overlayImg'/>
+    </div>
+    <div className='content'>
     <Router basename="/React-Quiz">
       <Routes>
         <Route path="/" element={<FirstPage />}></Route>
@@ -14,6 +20,9 @@ function App(){
         <Route path="/page/Thirdpage/Results" element={<ThirdPage/>}></Route> 
       </Routes>
     </Router>
+    </div>
+    </div>
+  
       
     );
 
